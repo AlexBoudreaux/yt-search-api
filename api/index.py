a3f989dcd625c580e84144039048078f16191f79
alex-boudreaux <Alex.Boudreaux@ibm.com>
2024-07-05 22:37:26 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,44 +1,28 @@
-# from fastapi import FastAPI, HTTPException
-# from fastapi.middleware.cors import CORSMiddleware
-# from functions import search_videos
-
-# app = FastAPI()
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# @app.get("/search")
-# async def search(query: str, top_k: int = 30, namespace: str = "All"):
-#     valid_namespaces = ["All", "Entree", "Side Dish", "Dessert", "Beverage", "Appetizer", "Snack", "Soup", "Salad", "Breakfast", "Condiment", "Dip", "Cocktail", "Other"]
-#     if namespace not in valid_namespaces:
-#         raise HTTPException(status_code=400, detail="Invalid namespace")
-#     try:
-#         results = search_videos(query, top_k, namespace)
-#         return results
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-# @app.get("/")
-# def check():
-#     return {"message": "Healthy"}
-
-#app.py
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from functions import search_videos
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get('/')
-def hello_world():
-    return "Hello,World"
+@app.get("/")
+async def root():
+    return {"message": "Hello, World"}
 
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app)
+@app.get("/search")
+async def search(query: str, top_k: int = 30, namespace: str = "All"):
+    valid_namespaces = ["All", "Entree", "Side Dish", "Dessert", "Beverage", "Appetizer", "Snack", "Soup", "Salad", "Breakfast", "Condiment", "Dip", "Cocktail", "Other"]
+    if namespace not in valid_namespaces:
+        raise HTTPException(status_code=400, detail="Invalid namespace")
+    try:
+        results = search_videos(query, top_k, namespace)
+        return results
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
